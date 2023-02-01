396c374aa0320eaaafc4052337f61111557d3f4b
I-Maged <magedhanafy@gmail.com>
2023-02-01 20:45:46 +0200

    user backend

--- server/server.js
+++ server/server.js
@@ -1,1 +1,23 @@
-console.log('Connected to server...');
+const express = require('express');
+require('dotenv').config();
+require('colors');
+const PORT = process.env.PORT || 8080;
+const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (_, res) => {
+  res.status(200).json({ message: 'Welcome to the Support System API' });
+});
+
+app.use('/api/users', require('./routes/userRoutes'));
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
